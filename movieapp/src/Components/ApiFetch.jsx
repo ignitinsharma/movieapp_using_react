aac3e80c01ed1f3c7d605cf843934e866d3895d4
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2022-11-23 22:39:31 +0530

    added

--- movieapp/src/Components/ApiFetch.jsx
+++ movieapp/src/Components/ApiFetch.jsx
@@ -4,10 +4,11 @@ import { useEffect } from "react";
 
 function ApiFetch() {
   const [data, setdata] = useState([]);
+  const [page, setpage] = useState(1)
 
-  function fetchdata() {
+  function fetchdata(page) {
     fetch(
-      `https://fathomless-everglades-39788.herokuapp.com/api/movies?_limit=5`
+      `https://fathomless-everglades-39788.herokuapp.com/api/movies?_limit=4&_page=${page}`
     )
       .then((res) => res.json())
       .then((res) => {
@@ -28,10 +29,21 @@ function ApiFetch() {
   */
 
   useEffect(() => {
-    fetchdata();
-  }, []);
+    fetchdata(page);
+  }, [page]);
 
-  return <div></div>;
+  return (
+    <div>
+      {data.map((el) => (
+        <div>
+          <button disabled={page==1} onClick={()=>setpage(page-1)}>Pevious</button>
+          <button disabled={page.length>=0}  onClick={()=>setpage(page+1)}>Next</button>
+          <p>{el.title}</p>
+          <img src={el.poster} alt="" />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default ApiFetch;
